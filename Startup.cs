4ad42514c7e5c2c9f6d343a6d528836c6e342231
Truitte White <125323071+truitte-white@users.noreply.github.com>
2024-07-26 13:50:39 -0400

    Add files via upload
    initial commit

---
+++ Startup.cs
@@ -0,0 +1,81 @@
+﻿using LibraryAPI.Data;
+using LibraryAPI.Helpers;
+using LibraryAPI.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using LibraryAPI.Middleware;
+using LibraryAPI.Models;
+using LibraryAPI.Extensions;
+
+namespace LibraryAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSingleton<TokenHelper>();
+            services.Configure<Books>(Configuration);
+
+            // Add CORS policy
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+
+            // Add any other services (e.g., authentication, etc.) here
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            // Redirect HTTP requests to HTTPS
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors("AllowSpecificOrigin");
+
+            // Use custom middleware
+            app.UseMiddleware<AuthMiddleware>();
+            app.UseMiddleware<LoggedInMiddleware>();
+
+            // Use authentication middleware if needed
+            // app.UseAuthentication();
+
+            // Use authorization middleware if needed
+            // app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
